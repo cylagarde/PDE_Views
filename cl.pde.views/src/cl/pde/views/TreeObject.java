76441eb2de49328925671731f671cc00669fd5f4
cla <cy.lagarde@gmail.com>
2018-01-14 12:52:43 +0100

    add equals/hashcode

--- cl.pde.views/src/cl/pde/views/TreeObject.java
+++ cl.pde.views/src/cl/pde/views/TreeObject.java
@@ -49,4 +49,48 @@ public class TreeObject implements IAdaptable
   {
     return null;
   }
+
+  /*
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null)? 0 : name.hashCode());
+    result = prime * result + ((data == null)? 0 : data.hashCode());
+    return result;
+  }
+
+  /*
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    TreeObject other = (TreeObject) obj;
+    if (name == null)
+    {
+      if (other.name != null)
+        return false;
+    }
+    else if (!name.equals(other.name))
+      return false;
+    if (data == null)
+    {
+      if (other.data != null)
+        return false;
+    }
+    else if (!data.equals(other.data))
+      return false;
+    return true;
+  }
+
 }
