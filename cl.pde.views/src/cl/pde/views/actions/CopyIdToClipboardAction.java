7bd4dc93dbb6030a8be232cc0e59ba5eae9f53ad
st20085 <cy.lagarde@gmail.com>
2018-02-17 08:21:38 +0100

    Use RTF format

--- cl.pde.views/src/cl/pde/views/actions/CopyIdToClipboardAction.java
+++ cl.pde.views/src/cl/pde/views/actions/CopyIdToClipboardAction.java
@@ -1,16 +1,24 @@
 package cl.pde.views.actions;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.pde.internal.ui.PDEPlugin;
 import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.RTFTransfer;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
+import cl.pde.views.Constants;
 import cl.pde.views.TreeObject;
-import cl.pde.views.Util;
 
 /**
  * The class <b>CopyIdToClipboardAction</b> allows to.<br>
@@ -24,8 +32,8 @@ public class CopyIdToClipboardAction extends AbstractTreeViewerAction
   public CopyIdToClipboardAction(AbstractTreeViewer treeViewer)
   {
     super(treeViewer);
-    setText("Copy id to clipboard");
-    setToolTipText("Copy id to clipboard");
+    setText("Copy id/version to clipboard");
+    setToolTipText("Copy id/version to clipboard");
     setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_TOOL_PASTE));
   }
 
@@ -33,21 +41,20 @@ public class CopyIdToClipboardAction extends AbstractTreeViewerAction
   public boolean isEnabled() {
     ISelection selection = treeViewer.getSelection();
     Object[] items = ((IStructuredSelection) selection).toArray();
-    if (items.length != 1)
+    if (items == null || items.length == 0)
       return false;
-    Object obj = items[0];
-    if (obj instanceof TreeObject)
-    {
-      TreeObject treeObject = (TreeObject) obj;
-      if (treeObject.data == null)
-        return false;
-      String id = Util.getId(treeObject.data);
-      if (id == null)
-        return false;
-      setText("Copy \"" + id + "\" to clipboard");
-      return true;
-    }
 
+    for(Object item : items)
+    {
+      if (item instanceof TreeObject)
+      {
+        TreeObject treeObject = (TreeObject) item;
+        if (treeObject.data == null)
+          continue;
+
+        return true;
+      }
+    }
     return false;
   }
 
@@ -55,20 +62,78 @@ public class CopyIdToClipboardAction extends AbstractTreeViewerAction
   public void run()
   {
     ISelection selection = treeViewer.getSelection();
-    Object obj = ((IStructuredSelection) selection).getFirstElement();
-    if (obj instanceof TreeObject)
-    {
-      TreeObject treeObject = (TreeObject) obj;
-      if (treeObject.data == null)
-        return;
-      String id = Util.getId(treeObject.data);
-      if (id == null)
-        return;
+    Object[] items = ((IStructuredSelection) selection).toArray();
 
-      Clipboard clipboard = new Clipboard(treeViewer.getControl().getDisplay());
-      TextTransfer textTransfer = TextTransfer.getInstance();
-      clipboard.setContents(new String[]{id}, new Transfer[]{textTransfer});
-      clipboard.dispose();
+    StringBuilder buffer = new StringBuilder(4096);
+    StringBuilder rtfBuffer = new StringBuilder(4096);
+
+    //
+    rtfBuffer.append("{\\rtf1");
+
+    rtfBuffer.append("{\\colortbl;");
+    // black
+    rtfBuffer.append("\\red0\\green0\\blue0;");
+    // COUNTER_STYLER
+    rtfBuffer.append("\\red0\\green127\\blue174;");
+
+    // colors
+    Map<Color, String> colorMap = new HashMap<>();
+    Stream.of(Constants.PDE_COLORS).filter(Objects::nonNull).distinct().forEach(color -> {
+      //
+      colorMap.put(color, "\\cf" + (colorMap.size() + 3));
+
+      rtfBuffer.append("\\red");
+      rtfBuffer.append(color.getRed());
+      rtfBuffer.append("\\green");
+      rtfBuffer.append(color.getGreen());
+      rtfBuffer.append("\\blue");
+      rtfBuffer.append(color.getBlue());
+      rtfBuffer.append(";");
+    });
+    rtfBuffer.append("}");
+
+    //
+    for(Object item : items)
+    {
+      TreeObject treeObject = (TreeObject) item;
+      if (treeObject.data == null)
+        continue;
+
+      String name = treeObject.name;
+      if (name == null)
+        name = PDEPlugin.getDefault().getLabelProvider().getText(treeObject.data);
+
+      buffer.append(name).append('\n');
+
+      //
+      if (treeObject.foreground != null)
+        rtfBuffer.append(colorMap.get(treeObject.foreground));
+      else
+        rtfBuffer.append("\\cf1"); //black
+
+      // version
+      int lastIndex = name.lastIndexOf(')');
+      int beginIndex = name.lastIndexOf('(', lastIndex);
+      if (lastIndex >= 0 && beginIndex >= 0)
+      {
+        rtfBuffer.append(name.substring(0, beginIndex));
+
+        // COUNTER_STYLER
+        rtfBuffer.append("\\cf2");
+        rtfBuffer.append(name.substring(beginIndex));
+      }
+      else
+        rtfBuffer.append(name);
+
+      rtfBuffer.append("\\line");
     }
+    rtfBuffer.append("}");
+
+    //
+    Clipboard clipboard = new Clipboard(treeViewer.getControl().getDisplay());
+    TextTransfer textTransfer = TextTransfer.getInstance();
+    RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+    clipboard.setContents(new Object[]{buffer.toString(), rtfBuffer.toString()}, new Transfer[]{textTransfer, rtfTransfer});
+    clipboard.dispose();
   }
 }
