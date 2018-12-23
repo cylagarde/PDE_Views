fbdbd521d10e73f8ff3677154b1395cdf75c06af
LAGARDE Cyril <cy.lagarde@gmail.com>
2018-12-23 19:43:56 +0100

    limit arraylist creation

--- cl.pde.views/src/cl/pde/views/TreeParent.java
+++ cl.pde.views/src/cl/pde/views/TreeParent.java
@@ -2,6 +2,7 @@ package cl.pde.views;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.pde.core.IModel;
@@ -13,7 +14,7 @@ import cl.pde.PDEViewActivator;
  */
 public class TreeParent extends TreeObject
 {
-  private ArrayList<TreeObject> children;
+  private List<TreeObject> children;
   public Runnable loadChildRunnable = null;
   boolean loaded = false;
   boolean sorted = false;
@@ -21,7 +22,6 @@ public class TreeParent extends TreeObject
   public TreeParent(String name, Object data)
   {
     super(name, data);
-    children = new ArrayList<>();
   }
 
   public TreeParent(String name)
@@ -31,6 +31,8 @@ public class TreeParent extends TreeObject
 
   public synchronized void addChild(TreeObject child)
   {
+    if (children == null)
+      children = new ArrayList<>(2);
     children.add(child);
     child.setParent(this);
     sorted = false;
@@ -38,7 +40,8 @@ public class TreeParent extends TreeObject
 
   public synchronized void removeChild(TreeObject child)
   {
-    children.remove(child);
+    if (children != null)
+      children.remove(child);
     child.setParent(null);
     sorted = false;
   }
@@ -46,18 +49,20 @@ public class TreeParent extends TreeObject
   public synchronized TreeObject[] getChildren()
   {
     loadChildren();
+    if (children == null)
+      return new TreeObject[0];
     return children.toArray(new TreeObject[children.size()]);
   }
 
   public synchronized boolean hasChildren()
   {
     loadChildren();
-    return children.size() > 0;
+    return children == null? false : !children.isEmpty();
   }
 
   synchronized void loadChildren()
   {
-    //    System.out.println("++loadChildren " + this + "  loadChildRunnable " + loadChildRunnable + "  loaded " + loaded);
+    // System.out.println("++loadChildren " + this + " loadChildRunnable " + loadChildRunnable + " loaded " + loaded);
     if (loadChildRunnable != null && !loaded)
     {
       loadChildRunnable.run();
@@ -72,7 +77,8 @@ public class TreeParent extends TreeObject
     super.reset();
     if (loadChildRunnable != null && loaded)
     {
-      children.clear();
+      if (children != null)
+        children.clear();
       loaded = false;
       sorted = false;
 
@@ -96,7 +102,8 @@ public class TreeParent extends TreeObject
   {
     if (sorted)
       return;
-    Collections.sort(children, TREEOBJECT_COMPARATOR);
+    if (children != null)
+      Collections.sort(children, TREEOBJECT_COMPARATOR);
     sorted = true;
   }
 }
